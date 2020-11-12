e918f5c0278cc656ae44ec8956b023d453acaf9a
Canorus <helios7157@gmail.com>
2020-11-12 13:11:04 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # split_download
-bash script downloading file in segments
+파일 쪼개 받기
+
+## 필수 설치
+
+- bash(>=3.2.57(1)-release)
+- gawk
+- curl
+
+## 사용법
+
+'''
+bash split_download.sh '다운로드링크' [조각갯수]
+'''
+
+## 유사 리포
+
+짜고 나서야 알았는데 [이런 게](https://github.com/antonyho/bash-split-download) 이미  있더라구요..
---
+++ split_download.sh
@@ -0,0 +1,31 @@
+URL="$1"
+size=`curl -s -I -L "$URL" | gawk -v IGNORECASE=1 '/^Content-Length/ { print $2 }'`
+size=${size//[ $'\001'-$'\037']} # drop any control character
+if [ -z $size ]; then # if file size is not available through header
+	echo 'file size unavailable, check if file is found'
+	echo 'terminating..'
+	exit
+fi
+if [ -z "$2" ] # second argument for number of segments
+then
+	split=5 # default segments are 5
+else
+	split=$2
+fi
+# split download with curl --range
+read -p 'filename(include extension): ' filename
+for (( i=0; i<$split; i++ ))
+do
+	k=$((size / $split))
+	s=$((k * $i))
+	if [[ $i -eq $((split - 1)) ]]; then
+		e=''
+	else
+		e=$((k * $((i + 1)) -1))
+	fi
+	`curl --range $s-$e -o $filename.part$i $URL` &
+done
+wait
+# merge part files
+`cat $filename.part? > $filename; rm $filename.part*`
+echo 'downloaded complete: ' $filename
