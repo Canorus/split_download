cdeffd268be3beeda1b33a6e5fb8540ac95876d8
canorus <helios7157@gmail.com>
2020-11-25 11:27:32 +0900

    fix filename with whitespace not saved correctly

--- split_download.sh
+++ split_download.sh
@@ -1,14 +1,14 @@
 URL="$1"
 size=`curl -s -I -L "$URL" | gawk -v IGNORECASE=1 '/^Content-Length/ { print $2 }'`
 size=${size//[ $'\001'-$'\037']} # drop any control character
+echo 'size ' $size
 if [ -z $size ]; then # if file size is not available through header
 	echo 'file size unavailable, check if file is found'
-	echo 'terminating..'
 	exit
 fi
-if [ -z "$2" ] # second argument for number of segments
+if [ -z "$2" ] # second argument for number of split files
 then
-	split=5 # default segments are 5
+	split=5
 else
 	split=$2
 fi
@@ -23,9 +23,9 @@ do
 	else
 		e=$((k * $((i + 1)) -1))
 	fi
-	`curl --range $s-$e -o $filename.part$i $URL` &
+	echo 'downloading part '$i $s $e
+	`curl --range $s-$e -o "$filename".part$i $URL` &
 done
 wait
 # merge part files
-`cat $filename.part? > $filename; rm $filename.part*`
-echo 'downloaded complete: ' $filename
+`cat "$filename".part? > "$filename"; rm "$filename".part*`
